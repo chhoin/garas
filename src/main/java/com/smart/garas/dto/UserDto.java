2aeb1367dce8b757026c81ac563ff7841c3fee70
chhoin <heistall9@gmail.com>
2018-11-09 14:51:46 +0700

    userDto

--- src/main/java/com/smart/garas/dto/UserDto.java
+++ src/main/java/com/smart/garas/dto/UserDto.java
@@ -1,17 +1,30 @@
 package com.smart.garas.dto;
 
-public class UserDto {
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class UserDto implements UserDetails{
+	
+	private static final long serialVersionUID = -4916946230339756522L;
+
 	private String autoNum;
+	
 	private String userCode;
-	private String userName;
-	private String passWord;
-	private int enabled;
-	private String name;
-	private String firstName;
-	private String lastName;
-	private String phone;
+	private String username;
+	private String password;
+	private	String phone;
 	private String address;
 	
+	private List<roleDto> roles;
+	private boolean accountNonExpired = true;
+    private boolean accountNonLocked = true;
+    private boolean credentialsNonExpired = true;
+    private boolean enabled = true;
+    
+    
 	public String getAutoNum() {
 		return autoNum;
 	}
@@ -24,42 +37,19 @@ public class UserDto {
 	public void setUserCode(String userCode) {
 		this.userCode = userCode;
 	}
-	public String getUserName() {
-		return userName;
+	@Override
+	public String getUsername() {
+		return username;
 	}
-	public void setUserName(String userName) {
-		this.userName = userName;
+	public void setUsername(String username) {
+		this.username = username;
 	}
-	public String getPassWord() {
-		return passWord;
+	@Override
+	public String getPassword() {
+		return password;
 	}
-	public void setPassWord(String passWord) {
-		this.passWord = passWord;
-	}
-	public int getEnabled() {
-		return enabled;
-	}
-	public void setEnabled(int enabled) {
-		this.enabled = enabled;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	public String getFirstName() {
-		return firstName;
-	}
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	public String getLastName() {
-		return lastName;
-	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
+	public void setPassword(String password) {
+		this.password = password;
 	}
 	public String getPhone() {
 		return phone;
@@ -74,14 +64,48 @@ public class UserDto {
 		this.address = address;
 	}
 	
-	/*
-	 * (non-Javadoc)
-	 * @see java.lang.Object#toString()
-	 */
-	@Override
-	public String toString() {
-		return "UserDto [autoNum=" + autoNum + ", userCode=" + userCode + ", userName=" + userName + ", passWord="
-				+ passWord + ", enabled=" + enabled + ", name=" + name + ", firstName=" + firstName + ", lastName="
-				+ lastName + ", phone=" + phone + ", address=" + address + "]";
+	
+	
+	
+	
+	public List<roleDto> getRoles() {
+		return roles;
 	}
+	public void setRoles(List<roleDto> roles) {
+		this.roles = roles;
+	}
+	@Override
+	public boolean isAccountNonExpired() {
+		return accountNonExpired;
+	}
+	public void setAccountNonExpired(boolean accountNonExpired) {
+		this.accountNonExpired = accountNonExpired;
+	}
+	@Override
+	public boolean isAccountNonLocked() {
+		return accountNonLocked;
+	}
+	public void setAccountNonLocked(boolean accountNonLocked) {
+		this.accountNonLocked = accountNonLocked;
+	}
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return credentialsNonExpired;
+	}
+	public void setCredentialsNonExpired(boolean credentialsNonExpired) {
+		this.credentialsNonExpired = credentialsNonExpired;
+	}
+	@Override
+	public boolean isEnabled() {
+		return enabled;
+	}
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return roles;
+	}
+	
+	
 }
